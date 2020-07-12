14df4fb04633779d94554efd51b7989a3aad39cf
mariehardy <mademoiselle.mph@gmail.com>
2020-07-12 16:08:50 +0200

    currentDay is not increased when already 30

--- routes/exercise-routes.js
+++ routes/exercise-routes.js
@@ -6,13 +6,11 @@ const Day = require('../models/day-model');
 const router = express.Router();
  
 // GET route => to retrieve a specific exercise
-// router.get('/day/:dayId/exercise/:exerciseId', (req, res, next) => {
 router.get('/exercise/:id', (req, res, next) => {
-  console.log('I am in Exercise route')
 
   Exercise.findById(req.params.id)
     .then(response => {
-      console.log('Exercise Route response is ======== ', response)
+      // console.log('Exercise Route response is ======== ', response)
       res.json(response);
     })
     .catch(error => {
