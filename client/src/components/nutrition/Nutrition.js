d77e38aff601d89cf9914f10a321f4b395bbfecd
Olga-OK <59260350+Olga-OK@users.noreply.github.com>
2020-07-21 22:24:55 +0200

    nutrition pages design

--- client/src/components/nutrition/Nutrition.js
+++ client/src/components/nutrition/Nutrition.js
@@ -1,17 +1,122 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import image1 from '../../assets/img/shapeIT/nutrition_main_first.jpg'
+import image2 from '../../assets/img/shapeIT/nutrition_main_second.jpg'
+import image3 from '../../assets/img/shapeIT/nutrition_main_third.jpg'
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardImgOverlay,
+  CardLink,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  ListGroupItem,
+  ListGroup
+} from "reactstrap";
 
 export class Nutrition extends Component {
   render() {
+
     return (
-      <div className="global-botton-margin">
-        NUTRITION MAIN PAGE
-        <br/>
-        <a href="/nutrition-basics">Nutrition Basics</a>
-        <br/>
-        <a href="/nutrition-how-to-eat">How to Eat?</a>
-        <br/>
-        <a href="/nutrition-healthy-meals">Meal Plans</a>
+
+      // <div className="nutritionWrapper">
+      //   <div className="nutrutuin_1">
+      //     <h3 className="nutrition_text">Nutrition Basics</h3>
+      //   </div>
+      //   <div className="nutrutuin_2">
+
+      //     <h3 className="nutrition_text">Nutrition2 Basics</h3>
+      //   </div>
+      //   <div className="nutrutuin_3">
+
+      //     <h3 className="nutrition_text">Nutrition 3Basics</h3>
+      //   </div>
+      // </div>
+
+      //   <div>
+      //   <Card inverse>
+      //     <CardImg className = "img_nutrition_size" height="100px" width="100%" src={image} alt="..." />
+      //     <CardImgOverlay>
+      //       <CardTitle>Card Title</CardTitle>
+      //       <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+      //       <CardText>
+      //         <small className="text-muted">Last updated 3 mins ago</small>
+      //       </CardText>
+      //     </CardImgOverlay>
+      //   </Card>
+      // </div>
+      <div>
+        <Card className="text-white text-right">
+          <CardImg
+            alt="..."
+            src={image1}
+          ></CardImg>
+          <CardImgOverlay>
+            <CardTitle tag="h4">Nutrition Basics</CardTitle>
+            <CardText>
+              <h5>Here you will find out practical recommendations for your nutrition adjustments</h5>
+          </CardText>
+            <Link to="/nutrition-basics" >
+              Get to know more
+          </Link>
+
+          </CardImgOverlay>
+        </Card>
+
+        <Card className="text-white text-middle">
+          <CardImg
+            alt="..."
+            src={image2}
+          ></CardImg>
+          <CardImgOverlay>
+            <CardTitle tag="h4">Nutrition Basics</CardTitle>
+            <CardText>
+              Here you will find out practical recommendations for your nutrition adjustments
+          </CardText>
+          <Link to="/nutrition-how-to-eat" >
+              Get to know more
+          </Link>
+
+          </CardImgOverlay>
+        </Card>
+
+        <Card className="text-white text-left">
+          <CardImg
+            alt="..."
+            src={image3}
+          ></CardImg>
+          <CardImgOverlay>
+            <CardTitle tag="h4">Nutrition Basics</CardTitle>
+            <CardText>
+              Here you will find out practical recommendations for your nutrition adjustments
+          </CardText>
+          <Link to="/nutrition-healthy-meals" >
+              Get to know more
+          </Link>
+
+
+          </CardImgOverlay>
+        </Card>
+
+
+
       </div>
+
+      // <div className="global-botton-margin">
+      //   NUTRITION MAIN PAGE
+      // <br />
+      //   <a href="/nutrition-basics">Nutrition Basics</a>
+      //   <br />
+      //   <a href="/nutrition-how-to-eat">How to Eat?</a>
+      //   <br />
+      //   <a href="/nutrition-healthy-meals">Meal Plans</a>
+      // </div> 
+
     )
   }
 }
